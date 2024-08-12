634b6e9618c9f1e33bf3725de9519d7df6490384
hawehbeh <wehbeh.hannah.collage@gmail.com>
2024-08-12 15:10:57 -0700

    Update SplitFrequency.py

--- Accelerometer Code and Vibration Data/SplitFrequency.py
+++ Accelerometer Code and Vibration Data/SplitFrequency.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import periodogram as psd
 
-n = int(input("How many splits? "))
+n = int(input("How many splits? "))  # How many parts should the data be split into? For 1s from 60s files, use 60.
 
 files = []
 data = []
 
+# Collect data
 file = input("What is the name of the file? ")
 files.append(file)
 data.append(np.load(file+".npy"))
@@ -16,13 +17,17 @@ chan = 1
 samplerate = 48000
 size = len(data[0])/n
 
+# Set up graph
 plt.ion()  # to run GUI even1t loop
 figure, ax = plt.subplots()
 plt.title(files[0], fontsize=20)
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("psd units")
+plt.grid(which="major", linestyle="-", color="black")
+plt.grid(which="minor", linestyle="--")
 
 
+# Graph data
 for i in range(n):
     f, p = psd(data[0][int(size*i):int(size*(i+1)), chan], fs=samplerate)
     plt.loglog(f, p)

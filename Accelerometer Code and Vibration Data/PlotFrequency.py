d589dd3206c2f7d3f54ba72f3d5d26bb545d48d5
hawehbeh <wehbeh.hannah.collage@gmail.com>
2024-08-12 14:59:33 -0700

    Update PlotFrequency.py

--- Accelerometer Code and Vibration Data/PlotFrequency.py
+++ Accelerometer Code and Vibration Data/PlotFrequency.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import periodogram as psd
 
-n = int(input("How many files? "))
+n = int(input("How many files? "))  # How many files to graph?
 
 for i in range(n):
+    # Open file
     file = input("What is the name of file " + str(i+1) + "? ")
     data = np.load(file+".npy")
     chan = 1
@@ -13,11 +14,14 @@ for i in range(n):
     mx = np.max(data)*1.10
     f, p = psd(data[:, chan], fs=samplerate)
 
+    # Plot data
     plt.ion()  # to run GUI even1t loop
     figure, ax = plt.subplots()
     plt.title(file, fontsize=20)
     plt.xlabel("Frequency (Hz)")
-    plt.ylabel("psd units")
+    plt.ylabel("g**2/Hz")
     plt.loglog(f, p)
+    plt.grid(which="major", linestyle="-", color="black")
+    plt.grid(which="minor", linestyle="--")
 
 plt.show(block=True)

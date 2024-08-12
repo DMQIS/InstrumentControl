5fa4541826b71b62e4c0348b5cae0968ea7ef978
hawehbeh <wehbeh.hannah.collage@gmail.com>
2024-08-12 14:57:53 -0700

    Update CompareFrequency.py

--- Accelerometer Code and Vibration Data/CompareFrequency.py
+++ Accelerometer Code and Vibration Data/CompareFrequency.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import periodogram as psd
 
-n = int(input("How many files? "))
+n = int(input("How many files? "))  # How many files to compare?
 
 files = []
 data = []
 lengths = []
 
+# Collect files
 for i in range(n):
     file = input("What is the name of file " + str(i+1) + "? ")
     files.append(file)
@@ -18,13 +19,16 @@ for i in range(n):
 chan = 1
 samplerate = 48000
 
+# Set up periodogram
 plt.ion()  # to run GUI event loop
 figure, ax = plt.subplots()
 plt.title(" ", fontsize=20)
 plt.xlabel("Frequency (Hz)")
-plt.ylabel("psd units")
+plt.ylabel("g**2/Hz")
+plt.grid(which="major", linestyle="-", color="black")
+plt.grid(which="minor", linestyle="--")
 
-
+# Graph data
 for i in range(n):
     f, p = psd(data[i][:, chan], fs=samplerate)
     plt.loglog(f, p, label=files[i])

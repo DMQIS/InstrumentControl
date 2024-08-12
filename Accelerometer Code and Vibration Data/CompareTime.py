b7cbb23eff7ca777ec1a90056675424e94f1ed88
hawehbeh <wehbeh.hannah.collage@gmail.com>
2024-08-12 15:05:21 -0700

    Update CompareTime.py

--- Accelerometer Code and Vibration Data/CompareTime.py
+++ Accelerometer Code and Vibration Data/CompareTime.py
@@ -3,23 +3,26 @@ import matplotlib.pyplot as plt
 
 print("NOTE: Only works if files are same length!!!")
 
-n = int(input("How many files? "))
+n = int(input("How many files? "))  # How many files to compare?
 
+files = []
 data = []
 lengths = []
 
+# Collect data
 for i in range(n):
     file = input("What is the name of file " + str(i+1) + "? ")
+    files.append(file)
     data.append(np.load(file+".npy"))
     lengths.append(len(data[i]))
 
 
 chan = 1
 samplerate = 48000
-mn = -1  # np.min(data)*1.10
-mx = 1  # np.max(data)*1.10
+mn = -1  # Graph minimum
+mx = 1  # Graph maximum
 
-
+# Set up graph
 plt.ion()  # to run GUI event loop
 figure, ax = plt.subplots()
 plt.title(" ", fontsize=20)
@@ -27,9 +30,10 @@ plt.xlabel("Time (s)")
 plt.ylabel("g")
 plt.axis([0, max(lengths)/samplerate, mn, mx])
 
-
+# Plot data
 for i in range(n):
     x = np.linspace(0, max(lengths)/samplerate, lengths[i])
-    line, = plt.plot(x, data[i][:, chan])
+    line, = plt.plot(x, data[i][:, chan], label=files[i])
 
+plt.legend()
 plt.show(block=True)

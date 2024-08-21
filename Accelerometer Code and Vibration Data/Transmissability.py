dbf50cbd89a1473146af16574e159ef7e66883fd
hawehbeh <wehbeh.hannah.collage@gmail.com>
2024-08-21 12:26:29 -0700

    Update Transmissability.py

--- Accelerometer Code and Vibration Data/Transmissability.py
+++ Accelerometer Code and Vibration Data/Transmissability.py
@@ -1,24 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import periodogram as psd
 from scipy.fft import fft, fftfreq
 
 
 chan = 1
 samplerate = 48000
-s = .1
+s = 1  # Sets time to split and average over
 
 plt.ion()  # to run GUI event loop
 figure, ax = plt.subplots()
 plt.title(" ", fontsize=20)
-plt.xlabel("Frequency (Hz)")
+plt.xlabel("Frequency [Hz]")
 plt.ylabel("Transmissibility")
 
 lst = []
 
 for i in range(2):
     # Open files
-    file = input("What is the name of file " + str(i + 1) + "? ")
+    if i == 0:
+        file = input("What is the name of the input file? ")
+    else:
+        file = input("What is the name of the output file? ")
+
     data = np.load(file + ".npy")
     splits = int(np.size(data[:, chan]) / samplerate / s)
 
@@ -26,7 +29,7 @@ for i in range(2):
         print("File is too short or time is too long. Please try again.")
         exit()
 
-    # Average and plot data
+    # Average data
     new = np.zeros(int(samplerate * s))
     for j in range(splits):
         q = fft(data[int(samplerate * s * j):int(samplerate * s * (j + 1)), chan])
@@ -34,23 +37,11 @@ for i in range(2):
     x = fftfreq(int(np.size(data[0:int(samplerate*s), chan])/1), 1/samplerate)
     lst.append(new)
 
-# f, p = psd(data[0][0:, chan], fs=samplerate)
-# p1 = fft(data[0][0:, chan])
-# x = fftfreq(int(len(data[0])/1), 1/samplerate)
-# print(f, p, x, p1)
-# f, q = psd(data[1][0:, chan], fs=samplerate)
-# q1 = fft(data[1][0:, chan])
-# x = fftfreq(int(len(data[1])/1), 1/samplerate)
-# print(f, q, x, q1)
+# Divide and plot data
 r = np.divide(lst[1], lst[0])
-# r1 = np.divide(q1, p1)
-# print(r, r1)
-# plt.loglog(f, r, color="pink", label="Periodogram")
-plt.loglog(x, np.abs(r), color="navy", label="Fourier Transform")
+plt.loglog(x, np.abs(r), color="navy")
 
 
 plt.grid(which="major", linestyle="-", color="black")
 plt.grid(which="minor", linestyle="--")
-
-plt.legend()
 plt.show(block=True)
